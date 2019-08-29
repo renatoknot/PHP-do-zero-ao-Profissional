d91d0fa49229726788e7ea5e928b6e829840f5d0
Renato Gonçalves <renatoslip@hotmail.com>
2019-08-28 21:48:07 -0300

    Primeiro commit

---
+++ 5 - JS basico/script.js
@@ -0,0 +1,8 @@
+function adicionarIngrediente() {
+    var ing = document.getElementById('ingrediente').value;
+    var listahtml = document.getElementById('lista').innerHTML;
+
+    listahtml = listahtml + '<li>'+ing+'</li>';
+
+    document.getElementById('lista').innerHTML = listahtml;
+}
