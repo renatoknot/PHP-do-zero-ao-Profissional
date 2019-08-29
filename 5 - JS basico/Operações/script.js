1e74b8e5fdf7bde21a9d549f7b987c565aae1792
Renato Gonçalves <renatoslip@hotmail.com>
2019-08-29 19:17:51 -0300

    commit all

---
+++ 5 - JS basico/Operações/script.js
@@ -0,0 +1,7 @@
+function multiplicar() {
+    let campo1 = Number(document.getElementById('campo1').value);
+    let campo2 = Number(document.getElementById('campo2').value);
+
+    let multiplicacao = campo1*campo2;
+    alert(multiplicacao);
+}
