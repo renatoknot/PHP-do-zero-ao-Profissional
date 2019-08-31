a601edde17c842c393c55f601daccb310e93bbae
Renato Gonçalves <renatoslip@hotmail.com>
2019-08-30 23:43:10 -0300

    eventos de mouse

---
+++ 6- JS Intermediário/EventosDeMouse/script.js
@@ -0,0 +1,32 @@
+function apertouMouse() {
+    console.log('Apertou o Mouse!');
+}
+
+function soltouMouse() {
+    console.log('Soltou o Mouse!');
+}
+
+function mouseEmCima() {
+    console.log('Mouse passou por cima!');
+}
+
+function mouseSaiuDeCima() {
+    console.log('Mouse saiu de cima!');
+}
+
+function moveuMouse() {
+    console.log('O mouse foi movido!')
+}
+    
+function mouseClicou() {
+    console.log('Clicou!')
+}
+
+function botaoDireito() {
+    console.log('Botão direito do Mouse!!')
+    return false; //para nao abrir o menu do botao direito
+}
+
+function cliqueDuplo() {
+    console.log('Clicou 2 vezes!')
+}
