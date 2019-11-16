252ee1b7d3812f038ea84924730b871b881a3d4d
Renato Gonçalves <renatoslip@hotmail.com>
2019-11-16 10:39:36 -0300

    atualizando php avancado

---
+++ 12 - PHP avançado/documentando/index.php
@@ -0,0 +1,21 @@
+<?
+
+    /*
+    * Classe para teste
+    *
+    * Esta classe vai pegar um valor X e somar com um valor Y
+
+    *@package ControleDeEstoque
+    * @author Renato <renatoslip@hotmail.com> 
+    *
+    *@param $x é o primeiro valor a ser somado
+    *@param $y é o segundo valor a ser somado
+    *@return float
+    */
+    class Teste {
+        private $nome;
+        
+        public function somar($x,$y) {
+            return $x + $y;
+        }
+    }
