b460c44612a9888f15c07d97174e119409709231
Renato Gonçalves <renatoslip@hotmail.com>
2019-11-30 16:03:36 -0300

    atualizando jquery

---
+++ 14 - Jquery/css_styling_dimensoes/script.js
@@ -0,0 +1,16 @@
+//  remover class
+// $('h1').removeClass('fundovermelho')
+
+// checando se o elemento possui uma classe
+// if($('h1').hasClass('fundovermelho')){ //retorna true ou false
+//     alert('tem sim!');
+// } else{
+//     alert('Tem não!');
+// }
+
+$('h1').css('color', '#0000FF'); // adiciona atributo css, parecido com o attr(2 parametros)
+// $('h1').css('font-size', '15px');
+$('button').css('border', '1px solid #000').css('background-color', 'red').css('color', '#fff')//adicionando varias propriedas
+
+
+$('input').addClass('campodetexto');
